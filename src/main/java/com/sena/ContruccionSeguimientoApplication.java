e004f82ed593979b56c8c7056738119b50193936
Fabril <adrianfabril@gmail.com>
2023-10-13 10:44:11 -0500

    first commit

---
+++ src/main/java/com/sena/ContruccionSeguimientoApplication.java
@@ -0,0 +1,13 @@
+package com.sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContruccionSeguimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContruccionSeguimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/ContruccionSeguimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContruccionSeguimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
